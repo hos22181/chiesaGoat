ae79df6b837fde106ceed880d2b157c04e627559
nabil <hos22181@spengergasse.at>
2023-09-18 00:08:40 +0200

    dybala goat

--- simple-intellij-project/src/Main.java
+++ simple-intellij-project/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("VLAHOVIC DA GOAT");
+        System.out.println("DYBALA IS THE AS ROMA GOAT");
     }
 }
