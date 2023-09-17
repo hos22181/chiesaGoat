773449353bf55c5fa425921c68c4203b3caf925f
nabil <hos22181@spengergasse.at>
2023-09-17 22:44:35 +0200

    changing sout text

--- simple-intellij-project/src/Main.java
+++ simple-intellij-project/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Chiesa is da Goat");
+        System.out.println("Chiesa ist der beste Spieler aller Zeiten");
     }
 }
