49440b24d993774ceb84a0365ace393f9b1e812b
nabil <hos22181@spengergasse.at>
2023-09-17 22:45:35 +0200

    vlahovic text change

--- simple-intellij-project/src/Main.java
+++ simple-intellij-project/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Chiesa ist der beste Spieler aller Zeiten");
+        System.out.println("VLAHOVIC DA GOAT");
     }
 }
